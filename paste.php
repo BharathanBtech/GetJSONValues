8f9cd3b56a5adacc5645c2e85d4a2e5835ee585d
BharathanBtech <bharathanbtech@gmail.com>
2023-07-27 17:01:40 +0530

    Create paste.php

---
+++ paste.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Paste a JSON File</title>
+</head>
+<body>
+    <h1>Paste a JSON File</h1>
+    <form action="process.php" method="post">
+        <textarea name="json_data" rows="10" cols="50"></textarea>
+        <input type="submit" name="next" value="NEXT" />
+    </form>
+</body>
+</html>

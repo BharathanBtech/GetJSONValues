c8b28cca5f52ddde0bd0acc9ef58ebbbcb80d609
BharathanBtech <bharathanbtech@gmail.com>
2023-07-27 17:00:59 +0530

    Create index.php
    creating the index page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSON Upload and Paste</title>
+</head>
+<body>
+    <h1>JSON File Handling</h1>
+    <ul>
+        <li><a href="upload.php">Upload a JSON File</a></li>
+        <li><a href="paste.php">Paste a JSON File</a></li>
+    </ul>
+</body>
+</html>
